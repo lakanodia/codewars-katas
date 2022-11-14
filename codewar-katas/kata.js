c874e4f25d998166f7c3a8b9435e06a23a7332c3
Laka Nodia <laka.nodia@gmail.com>
2022-11-14 11:27:42 +0400

    Add new challenge

--- codewar-katas/kata.js
+++ codewar-katas/kata.js
@@ -15,20 +15,32 @@
 
 // Challenge 3
 
-function toCamelCase(str) {
-  let newArr = [];
-  let array = str.split(/[-_]+/);
-  let firstWord = array[0];
-  let arrayTwo = array.splice(0, 1);
+// function toCamelCase(str) {
+//   let newArr = [];
+//   let array = str.split(/[-_]+/);
+//   let firstWord = array[0];
+//   let arrayTwo = array.splice(0, 1);
+//   array.forEach((item, index) => {
+//     let upperCaseWords = item.charAt(0).toUpperCase() + item.slice(1);
+//     newArr.push(upperCaseWords);
+//     return newArr;
+//   });
+//   let finalArray = newArr.unshift(firstWord);
+//   return newArr.join("");
+// }
+// let res = toCamelCase("the-Stealth-Warrior");
+// console.log(res);
 
-  array.forEach((item, index) => {
-    let upperCaseWords = item.charAt(0).toUpperCase() + item.slice(1);
-    newArr.push(upperCaseWords);
-    console.log(newArr);
-    return newArr;
-  });
-  let finalArray = newArr.unshift(firstWord);
-  return newArr.join("");
+// Challenge 4
+function solution(str) {
+  if (str.length % 2 == 0) {
+    const evenRestult = str.match(/.{1,2}/g) || [];
+    return evenRestult;
+  } else {
+    let newString = str.concat("_");
+    let oddResult = newString.match(/.{1,2}/g) || [];
+    return oddResult;
+  }
 }
-let res = toCamelCase("The-Stealth-Warrior");
+let res = solution("lakal");
 console.log(res);
